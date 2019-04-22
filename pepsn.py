482531c1e3326a24a15891e08326aaa692fde584
Prabhdeep Singh <prabhdeepsingh@prabhdeeps-mbp.attlocal.net>
2019-04-21 21:08:28 -0500

    twitter and sportsreference api added and working

--- pepsn.py
+++ pepsn.py
@@ -1,4 +1,31 @@
 from flask import Flask, render_template, url_for
+from datetime import datetime
+# Has one day old scores and sports info. nfl, bball
+from sportsreference.nba.boxscore import Boxscores
+import twitter
+import numpy as np
+# env file
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+# nba box score for yesterday
+games = Boxscores(datetime(2019, 4, 20))
+# print(games)
+# print(games.games)
+api = twitter.Api(consumer_key=os.getenv("TWITTER_CONSUMER_KEY"),
+                  consumer_secret=os.getenv("TWITTER_CONSUMER_SECRET"),
+                  access_token_key=os.getenv("TWITTER_ACCESS_TOKEN_KEY"),
+                  access_token_secret=os.getenv("TWITTER_ACCESS_TOKEN_SECRET"))
+
+# twitter calls
+ShamsCharania = api.GetUserTimeline(screen_name="ShamsCharania", count=5)
+wojespn = api.GetUserTimeline(screen_name="wojespn", count=5)
+nba = api.GetUserTimeline(screen_name="NBA", count=5)
+combined_tweets = ShamsCharania + wojespn + nba
+# shuffle tweets
+np.random.shuffle(combined_tweets)
+
 app = Flask(__name__)
 
 posts = [
@@ -19,7 +46,11 @@ posts = [
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html', posts=posts)
+    return render_template('home.html', tweets=combined_tweets)
+
+@app.route("/scores")
+def scores():
+    return render_template('scores.html', scores=games.games['4-20-2019'])
 
 
 @app.route("/login")
